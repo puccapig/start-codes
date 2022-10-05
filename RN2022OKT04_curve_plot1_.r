e269d5842dcc69b1a9aa8663d6ea0ee22e472760
Rachel Yuan Nong Mikkelsen <puccapig@gmail.com>
2022-10-05 22:36:48 +0800

    Add files via upload

---
+++ RN2022OKT04_curve_plot1_.r
@@ -0,0 +1,27 @@
+#Rachel Yuan Nong Mikkelsen 2022OCT04|happy cinnamonbunday!!!
+
+a <- c(1:10000)
+b <- c(1:10)
+col.ab <- rainbow(10)
+
+source("/Users/rachelnong/Desktop/paperdna_codes/select_n_cell.R")#500
+a <- a[o]
+
+dev.new()
+par(cex = 0.5)
+for(i in 1:10){
+	par(new = TRUE)
+	plot(a, log2(b[i]/a), xlab = "", ylab = "", ylim = c(-20, 0), xlim = c(1, 10000), col = col.ab[i])
+	}
+par(cex = 2)
+title(sub = "y ~ b/x | b in 1:10")
+
+legend.ab <- paste0("y ~ ", b, "/x")
+legend("topright", legend = legend.ab, col.text = col.ab, bty = "n")
+
+
+
+
+
+
+
