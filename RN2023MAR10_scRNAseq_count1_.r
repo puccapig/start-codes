3e2cf6724b7b13bc80ecab5ad2b1cf968586efe4
Rachel Yuan Nong Mikkelsen <puccapig@gmail.com>
2023-03-11 02:19:31 +0800

    Add files via upload

--- RN2023MAR10_scRNAseq_count1_.r
+++ RN2023MAR10_scRNAseq_count1_.r
@@ -69,12 +69,57 @@ rownames(table_a) <- table_a_index[row_start:row_stop]
 table_a[,3] <- round(table_a[,2]/sum(table_a[,2])*100)
 table_a[,4] <- paste0(rownames(table_a), ": ", table_a[, 3], "%")
 
-par(mfrow = c(1,4), cex = 0.5)
+a[,5] <- round(a[,2]/dim(mat1_)[2]*100)
+for(i in 1:dim(a)[1]){
+	if(a[i,5] >= 0 & a[i,5] <= 10){
+                a[i,6] <- c("a.1")
+                }
+        if(a[i,5] > 10 & a[i,5] <= 20){
+                a[i,6] <- c("a.2")
+                }
+        if(a[i,5] > 20 & a[i,5] <= 30){
+                a[i,6] <- c("a.3")
+                }
+        if(a[i,5] > 30 & a[i,5] <= 40){
+                a[i,6] <- c("a.4")
+                }
+        if(a[i,5] > 40 & a[i,5] <= 50){
+                a[i,6] <- c("a.5")
+                }
+        if(a[i,5] > 50 & a[i,5] <= 60){
+                a[i,6] <- c("a.6")
+                }
+	if(a[i,5] > 60 & a[i,5] <= 70){
+                a[i,6] <- c("a.7")
+                }
+        if(a[i,5] > 70 & a[i,5] <= 80){
+                a[i,6] <- c("a.8")
+                }
+        if(a[i,5] > 80 & a[i,5] <= 90){
+                a[i,6] <- c("a.9")
+                }
+        if(a[i,5] > 90 & a[i,5] <= 100){
+                a[i,6] <- c("a.10")
+                }
+}
+
+table_b <- as.data.frame(table(a[,6]))
+table_b_index <- c("(0-10%)", "(11-20%)", "(21-30%)", "(31-40%)", "(41-50%)", "(51-60%)", "(61-70%)", "(71-80%)", "(81-90%)", "(91-100%)")
+pick_row <- sapply(strsplit(as.character(table_b[,1]), "[.]"), function(x){x[2]})
+pick_row <- as.integer(as.character(pick_row))
+rownames(table_b) <- table_b_index[pick_row]
+table_b[,3] <- round(table_b[,2]/sum(table_b[,2])*100)
+table_b[,4] <- paste0(rownames(table_b), ": ", table_b[, 3], "%")
+
+par(mfrow = c(1,6), cex = 0.5)
 hist(a[,1], main = "expression > 1", xlab = "number of single cell")
 hist(a[,2], main = "expression == 1", xlab = "number of single cell")
-hist(a[,3], main = paste0("prop. count1 in n=", dim(mat1_)[2]), xlab = "%")
+hist(a[,3], main = paste0("prop. > count1 in n=", dim(mat1_)[2]), xlab = "%")
 legend("topleft", legend = table_a[,4], text.col = rainbow(dim(table_a)[1]), bty = "n")
-barplot(table_a[,3], col = rainbow(4), names.arg = rownames(table_a))
+barplot(table_a[,3], col = rainbow(dim(table_a)[1]), names.arg = rownames(table_a))
+hist(a[,5], main = "prop. count1", xlab = "%")
+legend("topleft", legend = table_b[,4], text.col = rainbow(dim(table_b)[1]), bty = "n")
+barplot(table_b[,3], col = rainbow(dim(table_b)[1]), names.arg = rownames(table_b))
 print("plot title_:")
 title_ <- readline()
 title(sub = title_)
